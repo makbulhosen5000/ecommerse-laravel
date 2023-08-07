ac93a02dee4e11d527162a4114607fd663313cb8
mh akash <mhakash5000@gmail.com>
2023-08-07 19:09:21 +0600

    added installtion,authentication and include all pages

---
+++ app/Http/Controllers/HomesController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cr;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index(){
+        return view('home.userpage');
+    }
+    public function redirect()
+    {
+       $usertype = Auth::user()->usertype;
+       if($usertype == '1'){
+        return view('admin.home');
+       }else{
+        return view('home.userpage');
+       }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function show(cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(cr $cr)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\HomesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/',[HomesController::class,'index']);
+Route::prefix('todos')->controller(HomesController::class)->name('todos.')->group(function () {   
+     Route::get('','index')->name('index');
+     Route::get('show/{slug}', ['show'])->name('show');
+     Route::get('edit/{slug}', ['edit'])->name('edit');
+});
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+Route::get('/redirect',[HomesController::class, 'redirect']);
